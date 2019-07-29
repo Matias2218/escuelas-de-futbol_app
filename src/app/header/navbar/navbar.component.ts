a56b369d11499e8a99021c60f9c2aa6287a49c96
Matias Maldonado <maldo1514@gmail.com>
2019-07-29 00:07:49 -0400

    commit 0:07 29/07/2019
    -Navbar Responsivo

--- src/app/header/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar.component.ts
@@ -1,4 +1,13 @@
-import {Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+  ViewChild
+} from '@angular/core';
 import {ModalTemplate, SuiModalService, TemplateModalConfig} from 'ng2-semantic-ui';
 
 export interface IContext {
@@ -10,23 +19,45 @@ export interface IContext {
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit {
 
   @ViewChild('divElement', {static: false}) div: ElementRef;
-
   @ViewChild('modalTemplate', {static: false})
-  public modalTemplate: ModalTemplate<IContext, string, string>
 
-  constructor(private renderer: Renderer2, private modalService: SuiModalService) { }
+  scrHeight: any;
+  scrWidth: any;
+  esResponsivo = false;
+
+  public modalTemplate: ModalTemplate<IContext, string, string>;
+
+  constructor(private renderer: Renderer2, private modalService: SuiModalService) {
+  }
+
   ngOnInit() {
     scroll();
   }
 
+  ngAfterViewInit(): void {
+    // Decidir el nav a utlizar
+    this.getScreenSize();
+  }
+
   @HostListener('window:scroll') scroll() {
-    if ( window.scrollX === 0 && window.scrollY === 0) {
+    if (window.scrollX === 0 && window.scrollY === 0) {
       this.renderer.removeClass(this.div.nativeElement, 'greysi');
     } else {
       this.renderer.addClass(this.div.nativeElement, 'greysi');
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.scrHeight = window.innerHeight;
+    this.scrWidth = window.innerWidth;
+    if (this.scrWidth <= '800') {
+      this.esResponsivo = true;
+    } else if (this.scrWidth > '800') {
+      this.esResponsivo = false;
     }
   }
 
@@ -35,13 +66,16 @@ export class NavbarComponent implements OnInit {
     const config = new TemplateModalConfig<IContext, string, string>(this.modalTemplate);
 
     config.closeResult = 'closed!';
-    config.context = { data: dynamicContent };
+    config.context = {data: dynamicContent};
 
     this.modalService
       .open(config)
       .onApprove(result => {
         console.log('aprobado');
       })
-      .onDeny(result => {  console.log('rechazado'); });
+      .onDeny(result => {
+        console.log('rechazado');
+      });
   }
+
 }
